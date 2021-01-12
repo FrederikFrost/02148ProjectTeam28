89ca300b294e5604119b7284ec73a495243c37bf
46480871 <eliasdk@yahoo.dk>
2021-01-12 14:32:08 +0100

    Removed name input from first frame

--- jSpace-Project/src/common/src/main/Menu.java
+++ jSpace-Project/src/common/src/main/Menu.java
@@ -11,7 +11,6 @@ import javax.swing.text.BadLocationException;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionListener;
@@ -36,24 +35,14 @@ public class Menu {
         int WIDTH = frame.getWidth();
         int HEIGHT = frame.getHeight();
 
-        JLabel nameInputLabel = new JLabel("Enter your name");
-        nameInputLabel.setHorizontalAlignment(JLabel.CENTER);
-
-        JTextField nameInput = new JTextField(20);
-
         JButton createGame = new JButton("Create Game");
         JButton joinGame = new JButton("Join Game");
         JButton playMusic = new JButton("Play Music");
 
-        // add text field and label to panel
-        frame.getContentPane().add(nameInput, BorderLayout.SOUTH);
-        frame.getContentPane().add(nameInputLabel, BorderLayout.SOUTH);
         frame.getContentPane().add(createGame, BorderLayout.SOUTH);
         frame.getContentPane().add(joinGame);
         frame.getContentPane().add(playMusic);
         frame.pack();
-
-        System.out.println(frame.getHeight() + " " +  frame.getWidth());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
@@ -90,16 +79,6 @@ public class Menu {
                 System.out.println("Joined Game");
             }  
             });  
-
-        nameInput.addActionListener(new AbstractAction()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                String content = nameInput.getText();
-                System.out.println(content + " just entered their name!");
-            }
-        });
 
         playMusic.addActionListener(new AbstractAction()
         {
@@ -188,6 +167,7 @@ public class Menu {
 
         newFrame.add(mainPanel);
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        newFrame.setLocationRelativeTo(null);
         newFrame.setSize(470, 300);
         newFrame.setVisible(true);
     }
